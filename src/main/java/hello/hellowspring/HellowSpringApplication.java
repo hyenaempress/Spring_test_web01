678b4a45d2837935c4d2cd53dd53e4b4e6264ae5
박수연 <catnaro16@gmail.com>
2020-08-18 20:43:53 +0900

    test

---
+++ src/main/java/hello/hellowspring/HellowSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.hellowspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellowspring/HellowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellowspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
