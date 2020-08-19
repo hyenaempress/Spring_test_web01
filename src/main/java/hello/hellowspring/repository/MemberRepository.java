0b6d28f4af94dd30e29165409ce23b7ac0aa3e23
박수연 <catnaro16@gmail.com>
2020-08-19 12:43:59 +0900

    domain repository 만들기

---
+++ src/main/java/hello/hellowspring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellowspring.repository;
+
+import hello.hellowspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+        Optional<Member> findById(Long id);
+        Optional<Member> findByName(String name);
+        List<Member> findAll();
+}
