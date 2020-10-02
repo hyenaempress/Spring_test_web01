c6e05a8196100ea96f062e2f91ee6ccc5868becc
hyenaenpress <catnaro16@gmail.com>
2020-10-02 21:07:21 +0900

    f

--- src/main/java/hello/hellowspring/controller/HelloController.java
+++ src/main/java/hello/hellowspring/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
 
+
     @GetMapping("hello")
         public String hello(Model model) {
         model.addAttribute("data", "hello!!");
